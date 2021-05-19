2f34dd175b896946583d22d7a713da1336d5680f
Diego <diegodiazh1994@gmail.com>
2021-05-19 14:08:16 -0500

    add application-properties

--- src/main/java/com/example/poke/pokeApi/controllers/PokeApiController.java
+++ src/main/java/com/example/poke/pokeApi/controllers/PokeApiController.java
@@ -2,6 +2,7 @@ package com.example.poke.pokeApi.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import com.example.poke.pokeApi.services.PokeApiService;
 
 
 @RestController
+@RequestMapping("/api")
 public class PokeApiController {
 
     @Autowired
